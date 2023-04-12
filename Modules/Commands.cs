7e5e2724d451aedbba0eaf982cd02d6ece045589
Abdul <flareonverm@outlook.com>
2023-04-13 00:30:50 +0100

    Updated commands
    Changes
    -Changed from if statement to switch statement in run command function
    +added chatgpt.

--- Modules/Commands.cs
+++ Modules/Commands.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+using OpenAI;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace Kuzu.Modules
 {
-
-
     class Commands
     {
-
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         private static extern bool SystemParametersInfo(uint uiAction, uint uiParam, string pvParam, uint fWinIni);
+
         public string Command;
         public bool AllowedToRun = false;
-       
+
         public Commands()
         {
             AllowedToRun = true;
         }
-
 
         public void setCommand(string cmd)
         {
@@ -37,31 +33,72 @@ namespace Kuzu.Modules
         void SetBackground(string path)
         {
             SystemParametersInfo(0x14, 0, path, 0x01);
-
         }
 
-        public void RunCommand()
+        public async void RunCommand()
         {
-            if (Command.Contains("bg"))
-            {
-                if(Command.Contains("Black"))
-                {
-                    SetBackground("C:/Users/abdul/source/repos/Kuzu/Modules/Backgrounds/Black.jpg");
-                }else if (Command.Contains("Mizuhara"))
-                {
-                    SetBackground("C:/Users/abdul/source/repos/Kuzu/Modules/Backgrounds/Mizuhara3.png");
+            string[] commandParts = Command.Split(' ');
 
-                }
+            switch (commandParts[0])
+            {
+                case "bg":
+                    if (commandParts.Length < 2)
+                    {
+                        Console.WriteLine("Invalid command syntax: bg [background]");
+                    }
+                    else if (commandParts[1].Equals("Black", StringComparison.OrdinalIgnoreCase))
+                    {
+                        SetBackground("C:/Users/abdul/source/repos/Kuzu/Modules/Backgrounds/Black.jpg");
+                    }
+                    else if (commandParts[1].Equals("Mizuhara", StringComparison.OrdinalIgnoreCase))
+                    {
+                        SetBackground("C:/Users/abdul/source/repos/Kuzu/Modules/Backgrounds/Mizuhara3.png");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid background: " + commandParts[1]);
+                    }
+                    break;
+                case "chatgpt":
+                    if (commandParts.Length < 2)
+                    {
+                        Console.WriteLine("Invalid command syntax: chatgpt [message]");
+                    }
+                    else
+                    {
+                        string message = string.Join(" ", commandParts.Skip(1));
+                        string response = await GetGPTResponseAsync(message);
+                        Console.WriteLine("ChatGPT: " + response);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Command not found");
+                    break;
+            }
+        }
+
+        private async Task<string> GetGPTResponseAsync(string message)
+        {
+            string apiKey = "YOUR_API_KEY_HERE";
+            string modelId = "gpt-3.5-turbo";
+            string prompt = "Conversation with ChatGPT:\n\nUser: " + message + "\nChatGPT:";
+
+            OpenAIRequest request = new OpenAIRequest(prompt, modelId, 1, 50, apiKey);
+            OpenAIResponse response = await OpenAICompletion.CreateCompletionAsync(request);
+
+            if (response != null && response.choices != null && response.choices.Count > 0)
+            {
+                return response.choices[0].text.Trim();
             }
             else
             {
-                Console.WriteLine("Commad not found");
-              //  Write();
+                return "Sorry, I could not generate a response.";
             }
         }
+
         public void AutomaticRun()
         {
-            while(AllowedToRun == true)
+            while (AllowedToRun)
             {
                 Write();
                 RunCommand();
